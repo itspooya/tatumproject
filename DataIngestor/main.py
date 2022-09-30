3a8e50301c75509a5eb4656e49b64cc81235e5b8
Itscalledpooya <fallenangel201190@gmail.com>
2022-09-30 20:55:31 +0330

    ✨ Introduced python scheduler in order to work like a cronjob without depending on any other outside help
    Updated requirements.txt
    Updated main.py

--- DataIngestor/main.py
+++ DataIngestor/main.py
@@ -1,8 +1,19 @@
+import atexit
 from src import DataIngestor
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+cron = BlockingScheduler()
+
+
+def main():
+    ingestor = DataIngestor()
+    file_name = ingestor.download()
+    ingestor.upload_file(file_name)
+
 
 if __name__ == '__main__':
-    data_ingestor = DataIngestor()
-    file_name = data_ingestor.download()
-    data_ingestor.upload_file(file_name)
-
-
+    main()
+    cron.add_job(main, 'interval', hours=24)
+    cron.start()
+    # Shut down the scheduler when exiting the app
+    atexit.register(lambda: cron.shutdown(wait=False))
